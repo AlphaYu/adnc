64d62debd642144e6ceab5b3d28c2dda1d61ad2b
Alpha.Yu <xyz199809@gmail.com>
2020-11-19 00:06:59 +0800

    修复登录日志一个bug

--- src/ServerApi/Portal/Adnc.Maint/Adnc.Maint.Application/Services/Logs/Dtos/LoginLogDto.cs
+++ src/ServerApi/Portal/Adnc.Maint/Adnc.Maint.Application/Services/Logs/Dtos/LoginLogDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Adnc.Application.Shared.Dtos;
 using Adnc.Maint.Core.Entities;
 
 namespace  Adnc.Maint.Application.Dtos
@@ -8,8 +9,24 @@ namespace  Adnc.Maint.Application.Dtos
     /// <summary>
     /// 登录日志
     /// </summary>
-    public class LoginLogDto : LoginLogSaveInputDto
+    public class LoginLogDto : BaseInputDto
     {
+        public string IP { get; set; }
 
+        public string Device { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Succeed { get; set; }
+
+        public long? UserId { get; set; }
+
+        public string Account { get; set; }
+
+        public string UserName { get; set; }
+
+        public string RemoteIpAddress { get; set; }
+
+        public DateTime? CreateTime { get; set; }
     }
 }
