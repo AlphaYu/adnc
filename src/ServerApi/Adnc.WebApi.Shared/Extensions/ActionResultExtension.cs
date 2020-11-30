a3f277cefd337e2d7c25da2cbd8a801937206d27
Alpha.Yu <xyz199809@gmail.com>
2020-11-30 19:56:05 +0800

    增加 ActionResultExtension.cs 文件

---
+++ src/ServerApi/Adnc.WebApi.Shared/Extensions/ActionResultExtension.cs
@@ -0,0 +1,12 @@
+﻿using Adnc.Application.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adnc.WebApi.Shared.Extensions
+{
+    public static class ActionResultExtension
+    {
+    }
+}
