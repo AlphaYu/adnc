26b3b56c7f8030f9f8a3a55f327f3cfea7025e23
Alpha.Yu <xyz199809@gmail.com>
2021-02-02 17:30:52 +0800

    sql 文件更新

--- doc/adnc_cus_dev.sql
+++ doc/adnc_cus_dev.sql
@@ -1,98 +1,82 @@
 /*
- Navicat MySQL Data Transfer
-
- Source Server         : 193.112.75.77
- Source Server Type    : MariaDB
- Source Server Version : 100504
- Source Host           : 193.112.75.77:13308
- Source Schema         : adnc_cus_dev
-
- Target Server Type    : MariaDB
- Target Server Version : 100504
- File Encoding         : 65001
-
- Date: 25/11/2020 20:12:55
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.5.8-MariaDB-1:10.5.8+maria~focal : Database - adnc_cus_dev
+*********************************************************************
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+/*!40101 SET NAMES utf8 */;
 
--- ----------------------------
--- Table structure for CusFinance
--- ----------------------------
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`adnc_cus_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `adnc_cus_dev`;
+
+/*Table structure for table `CusFinance` */
+
 DROP TABLE IF EXISTS `CusFinance`;
-CREATE TABLE `CusFinance`  (
-  `ID` bigint(20) NOT NULL,
-  `CreateBy` bigint(20) NULL DEFAULT NULL,
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `ModifyBy` bigint(20) NULL DEFAULT NULL,
-  `ModifyTime` datetime(6) NULL DEFAULT NULL,
-  `Account` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Balance` decimal(18, 2) NOT NULL,
-  PRIMARY KEY (`ID`) USING BTREE,
-  CONSTRAINT `FK_CusFinance_Customer_ID` FOREIGN KEY (`ID`) REFERENCES `Customer` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of CusFinance
--- ----------------------------
+CREATE TABLE `CusFinance` (
+  `Id` bigint(20) NOT NULL DEFAULT 0,
+  `CreateBy` bigint(20) DEFAULT NULL,
+  `CreateTime` datetime(6) DEFAULT NULL,
+  `ModifyBy` bigint(20) DEFAULT NULL,
+  `ModifyTime` datetime(6) DEFAULT NULL,
+  `Account` varchar(16) NOT NULL,
+  `Balance` decimal(18,2) NOT NULL,
+  PRIMARY KEY (`Id`) USING BTREE,
+  CONSTRAINT `FK_CusFinance_Customer_Id` FOREIGN KEY (`Id`) REFERENCES `Customer` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for CusTransactionLog
--- ----------------------------
-DROP TABLE IF EXISTS `CusTransactionLog`;
-CREATE TABLE `CusTransactionLog`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL,
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `CustomerId` bigint(20) NOT NULL,
-  `Account` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `ExchangeType` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `ExchageStatus` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `ChangingAmount` decimal(18, 2) NOT NULL,
-  `Amount` decimal(18, 2) NOT NULL,
-  `ChangedAmount` decimal(18, 2) NOT NULL,
-  `Remark` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+/*Data for the table `CusFinance` */
 
--- ----------------------------
--- Records of CusTransactionLog
--- ----------------------------
+/*Table structure for table `Customer` */
 
--- ----------------------------
--- Table structure for Customer
--- ----------------------------
 DROP TABLE IF EXISTS `Customer`;
-CREATE TABLE `Customer`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL,
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `ModifyBy` bigint(20) NULL DEFAULT NULL,
-  `ModifyTime` datetime(6) NULL DEFAULT NULL,
-  `Account` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Nickname` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `Realname` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of Customer
--- ----------------------------
+CREATE TABLE `Customer` (
+  `Id` bigint(20) NOT NULL DEFAULT 0,
+  `CreateBy` bigint(20) DEFAULT NULL,
+  `CreateTime` datetime(6) DEFAULT NULL,
+  `ModifyBy` bigint(20) DEFAULT NULL,
+  `ModifyTime` datetime(6) DEFAULT NULL,
+  `Account` varchar(16) NOT NULL,
+  `Nickname` varchar(16) NOT NULL,
+  `Realname` varchar(16) NOT NULL,
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for __EFMigrationsHistory
--- ----------------------------
-DROP TABLE IF EXISTS `__EFMigrationsHistory`;
-CREATE TABLE `__EFMigrationsHistory`  (
-  `MigrationId` varchar(95) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `ProductVersion` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`MigrationId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+/*Data for the table `Customer` */
 
--- ----------------------------
--- Records of __EFMigrationsHistory
--- ----------------------------
-INSERT INTO `__EFMigrationsHistory` VALUES ('20201124033501_20201124_init', '3.1.9');
+insert  into `Customer`(`Id`,`CreateBy`,`CreateTime`,`ModifyBy`,`ModifyTime`,`Account`,`Nickname`,`Realname`) values 
+(144487932374421504,1600000000000,'2021-02-02 17:03:27.790833',1600000000000,'2021-02-02 17:03:27.790838','alpha2008','测试','测试用户');
 
-SET FOREIGN_KEY_CHECKS = 1;
+/*Table structure for table `CusTransactionLog` */
+
+DROP TABLE IF EXISTS `CusTransactionLog`;
+
+CREATE TABLE `CusTransactionLog` (
+  `Id` bigint(20) NOT NULL DEFAULT 0,
+  `CreateBy` bigint(20) DEFAULT NULL,
+  `CreateTime` datetime(6) DEFAULT NULL,
+  `CustomerId` bigint(20) NOT NULL,
+  `Account` varchar(16) DEFAULT NULL,
+  `ExchangeType` varchar(3) DEFAULT NULL,
+  `ExchageStatus` varchar(3) DEFAULT NULL,
+  `ChangingAmount` decimal(18,2) NOT NULL,
+  `Amount` decimal(18,2) NOT NULL,
+  `ChangedAmount` decimal(18,2) NOT NULL,
+  `Remark` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `CusTransactionLog` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- doc/adnc_maint_dev.sql
+++ doc/adnc_maint_dev.sql
@@ -1,173 +1,106 @@
 /*
- Navicat MySQL Data Transfer
-
- Source Server         : 193.112.75.77
- Source Server Type    : MariaDB
- Source Server Version : 100504
- Source Host           : 193.112.75.77:13308
- Source Schema         : adnc_maint_dev
-
- Target Server Type    : MariaDB
- Target Server Version : 100504
- File Encoding         : 65001
-
- Date: 25/11/2020 20:12:29
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 10.5.8-MariaDB-1:10.5.8+maria~focal : Database - adnc_maint_dev
+*********************************************************************
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+/*!40101 SET NAMES utf8 */;
 
--- ----------------------------
--- Table structure for SysCfg
--- ----------------------------
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`adnc_maint_dev` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `adnc_maint_dev`;
+
+/*Table structure for table `SysCfg` */
+
 DROP TABLE IF EXISTS `SysCfg`;
-CREATE TABLE `SysCfg`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL COMMENT '创建人',
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `ModifyBy` bigint(20) NULL DEFAULT NULL COMMENT '最后更新人',
-  `ModifyTime` datetime(6) NULL DEFAULT NULL,
-  `CfgDesc` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `CfgName` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `CfgValue` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+
+CREATE TABLE `SysCfg` (
+  `Id` bigint(20) NOT NULL DEFAULT 0,
+  `CreateBy` bigint(20) DEFAULT NULL COMMENT '创建人',
+  `CreateTime` datetime(6) DEFAULT NULL,
+  `ModifyBy` bigint(20) DEFAULT NULL COMMENT '最后更新人',
+  `ModifyTime` datetime(6) DEFAULT NULL,
+  `CfgDesc` varchar(256) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `CfgName` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `CfgValue` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
   `IsDeleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1606303844002 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统参数' ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='系统参数';
 
--- ----------------------------
--- Records of SysCfg
--- ----------------------------
-INSERT INTO `SysCfg` VALUES (1606203371016, 1600000000000, '2020-11-24 15:36:11.262574', NULL, NULL, '', 'appname', 'adnc', 1);
-INSERT INTO `SysCfg` VALUES (1606303844001, NULL, NULL, 1600000000000, '2020-11-25 19:35:49.168030', '系统名称', 'app', 'adnc', 0);
+/*Data for the table `SysCfg` */
 
--- ----------------------------
--- Table structure for SysDict
--- ----------------------------
+insert  into `SysCfg`(`Id`,`CreateBy`,`CreateTime`,`ModifyBy`,`ModifyTime`,`CfgDesc`,`CfgName`,`CfgValue`,`IsDeleted`) values 
+(1606203371016,1600000000000,'2020-11-24 15:36:11.262574',NULL,NULL,'','appname','adnc',1),
+(1606303844001,1600000000000,'2021-02-02 12:50:45.430948',1600000000000,'2021-02-02 12:50:45.430949','系统名称','app','adnc',0),
+(1610294384001,1600000000000,'2021-01-10 23:59:44.212991',NULL,NULL,'1','66','kk',1),
+(1610295036002,NULL,NULL,1600000000000,'2021-01-11 00:10:41.953874','111','1111','9999',1);
+
+/*Table structure for table `SysDict` */
+
 DROP TABLE IF EXISTS `SysDict`;
-CREATE TABLE `SysDict`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL COMMENT '创建人',
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `ModifyBy` bigint(20) NULL DEFAULT NULL COMMENT '最后更新人',
-  `ModifyTime` datetime(6) NULL DEFAULT NULL,
-  `Name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `Num` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `Pid` bigint(20) NULL DEFAULT NULL,
-  `Tips` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+
+CREATE TABLE `SysDict` (
+  `Id` bigint(20) NOT NULL DEFAULT 0,
+  `CreateBy` bigint(20) DEFAULT NULL COMMENT '创建人',
+  `CreateTime` datetime(6) DEFAULT NULL,
+  `ModifyBy` bigint(20) DEFAULT NULL COMMENT '最后更新人',
+  `ModifyTime` datetime(6) DEFAULT NULL,
+  `Name` varchar(16) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `Num` varchar(16) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `Pid` bigint(20) DEFAULT NULL,
+  `Tips` varchar(64) CHARACTER SET utf8mb4 DEFAULT NULL,
   `IsDeleted` tinyint(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1606305946010 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '字典' ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='字典';
 
--- ----------------------------
--- Records of SysDict
--- ----------------------------
-INSERT INTO `SysDict` VALUES (1606304212002, 1600000000000, '2020-11-25 19:36:52.130272', 1600000000000, '2020-11-25 20:05:46.936175', '性别', '0', 0, NULL, 0);
-INSERT INTO `SysDict` VALUES (1606304212003, 1600000000000, '2020-11-25 19:36:52.130269', NULL, NULL, '男', '1', 1606304212002, NULL, 1);
-INSERT INTO `SysDict` VALUES (1606304212004, 1600000000000, '2020-11-25 19:36:52.130272', NULL, NULL, '女', '2', 1606304212002, NULL, 1);
+/*Data for the table `SysDict` */
 
--- ----------------------------
--- Table structure for SysLoginLog
--- ----------------------------
-DROP TABLE IF EXISTS `SysLoginLog`;
-CREATE TABLE `SysLoginLog`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL,
-  `Device` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `Message` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `Succeed` tinyint(1) NOT NULL,
-  `UserId` bigint(20) NULL DEFAULT NULL,
-  `Account` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `UserName` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `RemoteIpAddress` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 119530005997948929 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+insert  into `SysDict`(`Id`,`CreateBy`,`CreateTime`,`ModifyBy`,`ModifyTime`,`Name`,`Num`,`Pid`,`Tips`,`IsDeleted`) values 
+(1606304212002,1600000000000,'2020-11-25 19:36:52.130272',1600000000000,'2020-11-25 20:05:46.936175','性别','0',0,NULL,1),
+(1606304212003,1600000000000,'2020-11-25 19:36:52.130269',NULL,NULL,'男','1',1606304212002,NULL,1),
+(1606304212004,1600000000000,'2020-11-25 19:36:52.130272',NULL,NULL,'女','2',1606304212002,NULL,1),
+(1610295069003,1600000000000,'2021-01-11 00:11:09.465005',NULL,NULL,'9999','0',0,NULL,1),
+(1610295069004,1600000000000,'2021-01-11 00:11:09.465003',NULL,NULL,'8','8',1610295069003,NULL,1),
+(1612241328001,1600000000000,'2021-02-02 12:50:24.887549',1600000000000,'2021-02-02 12:50:24.887549','性别','0',0,NULL,0),
+(1612241328002,NULL,NULL,NULL,NULL,'男','1',1612241328001,NULL,1),
+(1612241328003,NULL,NULL,NULL,NULL,'女','2',1612241328001,NULL,1),
+(1612241346004,NULL,NULL,NULL,NULL,'性别','0',0,NULL,1),
+(1612241346005,NULL,NULL,NULL,NULL,'男','1',1612241346004,NULL,1),
+(1612241346006,NULL,NULL,NULL,NULL,'女','2',1612241346004,NULL,1),
+(1612241389007,NULL,NULL,NULL,NULL,'男','1',1612241328001,NULL,1),
+(1612241389008,NULL,NULL,NULL,NULL,'女','2',1612241328001,NULL,1),
+(1612241423009,NULL,NULL,NULL,NULL,'男','1',1612241328001,NULL,0),
+(1612241423010,NULL,NULL,NULL,NULL,'女','2',1612241328001,NULL,0);
 
--- ----------------------------
--- Records of SysLoginLog
--- ----------------------------
+/*Table structure for table `SysNotice` */
 
--- ----------------------------
--- Table structure for SysNotice
--- ----------------------------
 DROP TABLE IF EXISTS `SysNotice`;
-CREATE TABLE `SysNotice`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL,
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `ModifyBy` bigint(20) NULL DEFAULT NULL,
-  `ModifyTime` datetime(6) NULL DEFAULT NULL,
-  `Content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Type` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '通知' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of SysNotice
--- ----------------------------
-INSERT INTO `SysNotice` VALUES (1, 1, '2020-01-11 08:53:20.000000', 1, '2019-01-08 23:30:58.000000', 'Adnc是一个轻量级的.Net Core微服务开发框架', '新时代的.NET', 10);
--- ----------------------------
--- Table structure for SysTask
--- ----------------------------
-DROP TABLE IF EXISTS `SysTask`;
-CREATE TABLE `SysTask`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `CreateBy` bigint(20) NULL DEFAULT NULL COMMENT '创建人',
-  `CreateTime` datetime(6) NULL DEFAULT NULL,
-  `ModifyBy` bigint(20) NULL DEFAULT NULL COMMENT '最后更新人',
-  `ModifyTime` datetime(6) NULL DEFAULT NULL,
-  `Concurrent` bit(1) NULL DEFAULT NULL COMMENT '是否允许并发',
-  `Cron` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '定时规则',
-  `Data` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '执行参数',
-  `Disabled` bit(1) NULL DEFAULT NULL COMMENT '是否禁用',
-  `ExecAt` datetime(6) NULL DEFAULT NULL,
-  `ExecResult` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '执行结果',
-  `JobClass` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '执行类',
-  `JobGroup` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '任务组名',
-  `Name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '任务名',
-  `Note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '任务说明',
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 70962226407804929 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '定时任务' ROW_FORMAT = Dynamic;
+CREATE TABLE `SysNotice` (
+  `Id` bigint(20) NOT NULL DEFAULT 0,
+  `CreateBy` bigint(20) DEFAULT NULL,
+  `CreateTime` datetime(6) DEFAULT NULL,
+  `ModifyBy` bigint(20) DEFAULT NULL,
+  `ModifyTime` datetime(6) DEFAULT NULL,
+  `Content` varchar(255) DEFAULT NULL,
+  `Title` varchar(255) DEFAULT NULL,
+  `Type` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='通知';
 
--- ----------------------------
--- Records of SysTask
--- ----------------------------
+/*Data for the table `SysNotice` */
 
--- ----------------------------
--- Table structure for SysTaskLog
--- ----------------------------
-DROP TABLE IF EXISTS `SysTaskLog`;
-CREATE TABLE `SysTaskLog`  (
-  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ExecAt` datetime(6) NULL DEFAULT NULL,
-  `ExecSuccess` bit(1) NULL DEFAULT NULL COMMENT '执行结果（成功:1、失败:0)',
-  `IdTask` bigint(20) NULL DEFAULT NULL,
-  `JobException` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '抛出异常',
-  `Name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '任务名',
-  PRIMARY KEY (`ID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '定时任务日志' ROW_FORMAT = Dynamic;
+insert  into `SysNotice`(`Id`,`CreateBy`,`CreateTime`,`ModifyBy`,`ModifyTime`,`Content`,`Title`,`Type`) values 
+(1,1,'2020-01-11 08:53:20.000000',1,'2019-01-08 23:30:58.000000','Adnc是一个轻量级的.Net Core微服务开发框架','新时代的.NET',10);
 
--- ----------------------------
--- Records of SysTaskLog
--- ----------------------------
 
--- ----------------------------
--- Table structure for __EFMigrationsHistory
--- ----------------------------
-DROP TABLE IF EXISTS `__EFMigrationsHistory`;
-CREATE TABLE `__EFMigrationsHistory`  (
-  `MigrationId` varchar(95) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `ProductVersion` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`MigrationId`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of __EFMigrationsHistory
--- ----------------------------
-INSERT INTO `__EFMigrationsHistory` VALUES ('20201014072217_2020101401', '3.1.9');
-INSERT INTO `__EFMigrationsHistory` VALUES ('20201014152042_2020101402', '3.1.9');
-INSERT INTO `__EFMigrationsHistory` VALUES ('20201123161154_20201123_01', '3.1.9');
-INSERT INTO `__EFMigrationsHistory` VALUES ('20201124032950_20201124_1', '3.1.9');
-
-SET FOREIGN_KEY_CHECKS = 1;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
