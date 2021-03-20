285fa0d89bb1fafa785eab507d932ed6b5bab6fd
Alpha.Yu <xyz199809@gmail.com>
2021-03-20 21:56:57 +0800

    CreateBy,CreateTime 改为不可空。

--- src/ServerApi/Portal/Adnc.Cus/Adnc.Cus.Migrations/Migrations/20210320133554_Update2021032001.Designer.cs
+++ src/ServerApi/Portal/Adnc.Cus/Adnc.Cus.Migrations/Migrations/20210320133554_Update2021032001.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Adnc.Infr.EfCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Adnc.Cus.Migrations.Migrations
 {
     [DbContext(typeof(AdncDbContext))]
-    partial class AdncDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210320133554_Update2021032001")]
+    partial class Update2021032001
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/ServerApi/Portal/Adnc.Cus/Adnc.Cus.Migrations/Migrations/20210320133554_Update2021032001.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Adnc.Cus.Migrations.Migrations
+{
+    public partial class Update2021032001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "CustomerTransactionLog",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateBy",
+                table: "CustomerTransactionLog",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "CustomerFinance",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateBy",
+                table: "CustomerFinance",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "Customer",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateBy",
+                table: "Customer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "CustomerTransactionLog",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateBy",
+                table: "CustomerTransactionLog",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "CustomerFinance",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateBy",
+                table: "CustomerFinance",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreateTime",
+                table: "Customer",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CreateBy",
+                table: "Customer",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long));
+        }
+    }
+}
