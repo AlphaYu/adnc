fe071f6cf26d19f272651f9d351e504e016e0415
Alpha.Yu <xyz199809@gmail.com>
2021-11-06 19:08:05 +0800

    EntityTypeConfiguration.Configure 分离ConfigureKey,ConfigureConcurrency,ConfigureQueryFilter 3个虚方法，方便子类重写。

--- src/ServerApi/Infrastructures/Adnc.Infra.Repository/Entities/EfEnities/Config/EntityTypeConfiguration.cs
+++ src/ServerApi/Infrastructures/Adnc.Infra.Repository/Entities/EfEnities/Config/EntityTypeConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 
 namespace Adnc.Infra.Entities.Config
 {
@@ -9,15 +10,25 @@ namespace Adnc.Infra.Entities.Config
         public virtual void Configure(EntityTypeBuilder<TEntity> builder)
         {
             var entityType = typeof(TEntity);
+            ConfigureKey(builder, entityType);
+            ConfigureConcurrency(builder, entityType);
+            ConfigureQueryFilter(builder, entityType);
+        }
 
+        protected virtual void ConfigureKey(EntityTypeBuilder<TEntity> builder,Type entityType)
+        {
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedNever();
+        }
 
+        protected virtual void ConfigureConcurrency(EntityTypeBuilder<TEntity> builder, Type entityType)
+        {
             if (typeof(IConcurrency).IsAssignableFrom(entityType))
-            {
                 builder.Property("RowVersion").IsRequired().IsRowVersion().ValueGeneratedOnAddOrUpdate();
-            }
+        }
 
+        protected virtual void ConfigureQueryFilter(EntityTypeBuilder<TEntity> builder, Type entityType)
+        {
             if (typeof(ISoftDelete).IsAssignableFrom(entityType))
             {
                 builder.Property("IsDeleted")
