a8a77fe13b9cf8283b71e0d2eca803a3ded34edd
Alpha.Yu <xyz199809@gmail.com>
2022-04-03 23:32:40 +0800

    新增 IServiceCollection.AddAdncMapper() 扩展方法。

---
+++ src/ServerApi/Infrastructures/Adnc.Infra.Mapper/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Adnc.Infra.Mapper;
+using Adnc.Infra.Mapper.AutoMapper;
+using AutoMapper;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddAdncMapper(this IServiceCollection services, params Type[] profileAssemblyMarkerTypes)
+    {
+        services.AddAutoMapper(profileAssemblyMarkerTypes);
+        services.AddScoped<IObjectMapper, AutoMapperMapperImpl>();
+        return services;
+    }
+}
