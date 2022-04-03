d41969d7a1b27585a89cd4b1a6733662bfa530cc
Alpha.Yu <xyz199809@gmail.com>
2022-04-03 22:53:39 +0800

    新增 IServiceCollection.AddAdncEfRepositries() 扩展方法。

---
+++ src/ServerApi/Infrastructures/Adnc.Infra.EfCore.MySQL/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Adnc.Infra.EfCore;
+using Adnc.Infra.EfCore.MySQL;
+using Adnc.Infra.EfCore.Repositories;
+using Adnc.Infra.Entities;
+using Adnc.Infra.IRepositories;
+using System;
+using System.Reflection;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class AdncEfCoreMySqlServiceCollectionExtension
+{
+    public static IServiceCollection AddAdncEfRepositries(this IServiceCollection services, Assembly entitiesAssemblieToScan)
+    {
+        services.AddScoped<UnitOfWorkStatus>();
+        services.AddScoped<IUnitOfWork, UnitOfWork<AdncDbContext>>();
+        services.AddScoped(typeof(EfRepository<>), typeof(IEfRepository<>));
+        services.AddScoped(typeof(EfBasicRepository<>), typeof(IEfBasicRepository<>));
+        services.Scan(scan => scan.FromAssemblyOf<AdncDbContext>()
+                        .AddClasses(c => c.AssignableTo(typeof(IRepository<>)))
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime());
+        services.Scan(scan => scan.FromAssemblies(entitiesAssemblieToScan)
+                        .AddClasses(c => c.AssignableTo<IEntityInfo>())
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime());
+
+        return services;
+    }
+}
