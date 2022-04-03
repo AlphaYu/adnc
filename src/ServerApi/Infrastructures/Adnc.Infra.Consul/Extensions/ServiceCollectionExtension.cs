f240ddf0981ea2bb60d637e9e2a1b3407753eb97
Alpha.Yu <xyz199809@gmail.com>
2022-04-03 22:56:46 +0800

    新增 IServiceCollection.AddAdncConsul() 扩展方法。

---
+++ src/ServerApi/Infrastructures/Adnc.Infra.Consul/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Adnc.Infra.Consul.Consumer;
+using Adnc.Infra.Consul.TokenGenerator;
+using Consul;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class AdncInfraConsulServiceCollectionExtension
+{
+    public static IServiceCollection AddAdncConsul(this IServiceCollection services, string consulAddress)
+    {
+        services.AddScoped<ITokenGenerator, DefaultTokenGenerator>();
+        services.AddScoped<SimpleDiscoveryDelegatingHandler>();
+        services.AddScoped<ConsulDiscoverDelegatingHandler>();
+        services.AddSingleton(x => new ConsulClient(cfg => cfg.Address = new System.Uri(consulAddress)));
+        return services;
+    }
+}
