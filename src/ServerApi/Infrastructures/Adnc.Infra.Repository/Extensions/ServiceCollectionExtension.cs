fb7783b6ef107234cb4055401d3b2785febfbcf2
Alpha.Yu <xyz199809@gmail.com>
2022-04-03 22:58:00 +0800

    新增 IServiceCollection.AddAdncRepositry 扩展方法。

---
+++ src/ServerApi/Infrastructures/Adnc.Infra.Repository/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using Adnc.Infra.Entities;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class AdncRepositoryServiceCollectionExtension
+{
+    public static IServiceCollection AddAdncRepositry(this IServiceCollection services)
+    {
+        services.Scan(scan => scan
+        .FromCallingAssembly()
+        .AddClasses(c => c.AssignableTo<IEntityInfo>())
+        .AsImplementedInterfaces()
+        .WithScopedLifetime());
+
+        return services;
+    }
+}
