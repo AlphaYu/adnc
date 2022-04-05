2dada81bdc38429d5fdb8b8604257e1375319638
Alpha.Yu <xyz199809@gmail.com>
2022-04-05 14:29:19 +0800

    SystemTextJson代码格式调整

--- src/ServerApi/Infrastructures/Adnc.Infra.Core/Adnc/Json/SystemTextJson.cs
+++ src/ServerApi/Infrastructures/Adnc.Infra.Core/Adnc/Json/SystemTextJson.cs
@@ -2,27 +2,24 @@
 using System.Text.Json;
 using System.Text.Unicode;
 
-namespace Adnc.Infra.Core
-{
-    public static class SystemTextJson
-    {
-        public static JsonSerializerOptions GetAdncDefaultOptions()
-             => new JsonSerializerOptions
-             {
-                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                ,
-                 Encoder = GetAdncDefaultEncoder()
-                ,
-                 //该值指示是否允许、不允许或跳过注释
-                 ReadCommentHandling = JsonCommentHandling.Skip
-                ,
-                 //dynamic与匿名类型序列化设置
-                 PropertyNameCaseInsensitive = true
-                ,
-                 DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
-             };
+namespace Adnc.Infra.Core;
 
-        public static JavaScriptEncoder GetAdncDefaultEncoder()
-            => JavaScriptEncoder.Create(new TextEncoderSettings(UnicodeRanges.All));
-    }
+public static class SystemTextJson
+{
+    public static JsonSerializerOptions GetAdncDefaultOptions() => new()
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            ,
+        Encoder = GetAdncDefaultEncoder()
+            ,
+        //该值指示是否允许、不允许或跳过注释
+        ReadCommentHandling = JsonCommentHandling.Skip
+            ,
+        //dynamic与匿名类型序列化设置
+        PropertyNameCaseInsensitive = true
+            ,
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    public static JavaScriptEncoder GetAdncDefaultEncoder() => JavaScriptEncoder.Create(new TextEncoderSettings(UnicodeRanges.All));
 }
