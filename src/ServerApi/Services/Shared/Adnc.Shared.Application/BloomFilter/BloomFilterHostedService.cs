5cdba3d60be4eec545a678dd4cd6c046a68e14d6
Alpha.Yu <xyz199809@gmail.com>
2022-04-13 17:31:35 +0800

    移除BloomFilterHostedService.cs重复代码

--- src/ServerApi/Services/Shared/Adnc.Shared.Application/BloomFilter/BloomFilterHostedService.cs
+++ src/ServerApi/Services/Shared/Adnc.Shared.Application/BloomFilter/BloomFilterHostedService.cs
@@ -3,19 +3,13 @@
 public class BloomFilterHostedService : BackgroundService
 {
     private readonly ILogger<BloomFilterHostedService> _logger;
-    private readonly ICacheProvider _cache;
     private readonly IEnumerable<IBloomFilter> _bloomFilters;
-    private readonly ICachePreheatable _cacheService;
 
     public BloomFilterHostedService(ILogger<BloomFilterHostedService> logger
-       , ICacheProvider cache
-       , IEnumerable<IBloomFilter> bloomFilters
-       , ICachePreheatable cacheService)
+       , IEnumerable<IBloomFilter> bloomFilters)
     {
         _logger = logger;
-        _cache = cache;
         _bloomFilters = bloomFilters;
-        _cacheService = cacheService;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -31,43 +25,5 @@ public class BloomFilterHostedService : BackgroundService
         }
 
         #endregion Init BloomFilter
-
-        #region Init Caches
-
-        await _cacheService.PreheatAsync();
-
-        #endregion Init Caches
-
-        #region Confirm Caching Removed
-
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (!LocalVariables.Instance.Queue.TryDequeue(out LocalVariables.Model model)
-                || model.CacheKeys?.Any() == false
-                || DateTime.Now > model.ExpireDt)
-            {
-                await Task.Delay(_cache.CacheOptions.LockMs, stoppingToken);
-                continue;
-            }
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                try
-                {
-                    if (DateTime.Now > model.ExpireDt) break;
-
-                    await _cache.RemoveAllAsync(model.CacheKeys);
-
-                    break;
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(ex.Message, ex);
-                    await Task.Delay(_cache.CacheOptions.LockMs, stoppingToken);
-                }
-            }
-        }
-
-        #endregion Confirm Caching Removed
     }
 }
