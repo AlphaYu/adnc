80fd105cf3c76c502a3b533b0d4aa5b03b16dbdb
alpha.yu <xyz199809@gmail.com>
2022-05-18 01:14:49 +0800

    1、Adnc.Infra.Gateway 移到到Gateways目录，改名为Adnc.Ocelot 2、删除鸡肋工厂Adnc.Infra.HealthCheckUI

--- src/ServerApi/Infrastructures/Adnc.Infra.HealthCheckUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Adnc.Maintaining.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
