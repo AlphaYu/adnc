85c964702658b5c1b4570de0fd1a3803d9b10b50
garfiled huang <google_2009@163.com>
2022-05-20 17:56:18 +0800

    修改工程启动的bat文件

--- src/ServerApi/build/00_g_ocelot_runner.bat
+++ src/ServerApi/build/00_g_ocelot_runner.bat
@@ -1,4 +1,3 @@
-@echo off
-cd F:\github\Adnc\src\ServerApi\Gateways\Adnc.Ocelot\bin\Debug\net6.0
-cd f:
+cd %cd%
+cd ..\Gateways\Adnc.Ocelot\bin\Debug\net6.0
 dotnet Adnc.Ocelot.dll --urls "http://localhost:5002"
--- src/ServerApi/build/03_cust_runner.bat
+++ src/ServerApi/build/03_cust_runner.bat
@@ -1,4 +1,3 @@
-@echo off
-cd F:\github\Adnc\src\ServerApi\Services\Adnc.Cus\Adnc.Cus.WebApi\bin\Debug\net6.0
-cd f:
+cd %cd%
+cd ..\Services\Adnc.Cus\Adnc.Cus.WebApi\bin\Debug\net6.0
 dotnet Adnc.Cus.WebApi.dll
