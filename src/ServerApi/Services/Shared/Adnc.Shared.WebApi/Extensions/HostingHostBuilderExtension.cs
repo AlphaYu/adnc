fa3a6036b60d61c8645c032130d62ef2f7eef3ef
alpha.yu <xyz199809@gmail.com>
2022-06-13 18:32:01 +0800

    [feat]新增nlog-console.config文件,默认日志调整为console [feat]实现configrution占位符替换功能

--- src/ServerApi/Services/Shared/Adnc.Shared.WebApi/Extensions/HostingHostBuilderExtension.cs
+++ src/ServerApi/Services/Shared/Adnc.Shared.WebApi/Extensions/HostingHostBuilderExtension.cs
@@ -3,61 +3,91 @@ using NLog.Web;
 
 namespace Microsoft.Extensions.Hosting;
 
-public static class HostingHostBuilderExtension
+public static class WebApplicationBuilderExtension
 {
     public static WebApplicationBuilder ConfigureAdncDefault(this WebApplicationBuilder builder, string[] args, Assembly webApiAssembly)
     {
-        if (builder is null) throw new ArgumentNullException(nameof(builder));
+        if (builder is null) 
+            throw new ArgumentNullException(nameof(builder));
+
+        _serviceInfo = new ServiceInfo(webApiAssembly, builder.Environment);
+        var initialData = new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("ServiceName", _serviceInfo.ServiceName) };
 
         // Configuration
         builder.Services.ReplaceConfiguration(builder.Configuration);
         builder.Configuration.AddCommandLine(args);
-
+        builder.Configuration.AddInMemoryCollection(initialData);
         if (builder.Environment.IsDevelopment())
+        {
             builder.Configuration.AddJsonFile($"{AppContext.BaseDirectory}/appsettings.shared.development.json", true, true);
-
-        if (builder.Environment.IsProduction() || builder.Environment.IsStaging())
+        }
+        else if (builder.Environment.IsProduction() || builder.Environment.IsStaging())
         {
             var consulOption = builder.Configuration.GetConsulSection().Get<ConsulConfig>();
             builder.Configuration.AddConsulConfiguration(consulOption, true);
         }
-
-        var children = builder.Configuration.GetChildren();
-        var serviceInfo = new ServiceInfo(webApiAssembly, builder.Environment);
-        SetVariableValue(children, serviceInfo);
+        OnSettingConfigurationChanged(builder.Configuration);
 
         //ServiceCollection
-        builder.Services.AddSingleton(typeof(IServiceInfo), serviceInfo);
+        builder.Services.AddSingleton(typeof(IServiceInfo), _serviceInfo);
         builder.Services.AddAdnc(webApiAssembly);
 
         //Logging
-        builder.Logging.ClearProviders().AddConsole();
-        var logContainer = builder.Configuration.GetValue("Logging:LogContainer", string.Empty);
-        if (logContainer.IsNotNullOrWhiteSpace())
-            LogManager.LoadConfiguration($"{AppContext.BaseDirectory}/NLog/nlog-{logContainer}.config");
+        builder.Logging.ClearProviders();
+        var logContainer = builder.Configuration.GetValue("Logging:LogContainer", "console");
+        LogManager.LoadConfiguration($"{AppContext.BaseDirectory}/NLog/nlog-{logContainer}.config");
         builder.Host.UseNLog();
 
         //WebHost(Kestrel)
-        builder.WebHost.ConfigureKestrel((context, serverOptions) => serverOptions.Configure(context.Configuration.GetKestrelSection()));
+        builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+        {
+            var kestrelSection = context.Configuration.GetKestrelSection();
+            serverOptions.Configure(kestrelSection);
+        });
 
         return builder;
     }
 
-    private static void SetVariableValue(IEnumerable<IConfigurationSection> sections, IServiceInfo serviceInfo)
+    /// <summary>
+    /// replace placeholder
+    /// </summary>
+    /// <param name="sections"></param>
+    /// <param name="serviceInfo"></param>
+    private static void ReplacePlaceholder(IEnumerable<IConfigurationSection> sections, IServiceInfo serviceInfo)
     {
-        sections.ForEach(section =>
+        foreach (var section in sections)
         {
             var childrenSections = section.GetChildren();
             if (childrenSections.IsNotNullOrEmpty())
-                SetVariableValue(childrenSections, serviceInfo);
+                ReplacePlaceholder(childrenSections, serviceInfo);
 
             if (section.Value.IsNullOrWhiteSpace())
-                return;
+                continue;
 
-            if (section.Value.Equals("$ServiceName"))
-                section.Value = serviceInfo.ServiceName;
-            else if (section.Value.Equals("$ShortName"))
-                section.Value = serviceInfo.ShortName;
-        });
+            var sectionValue = section.Value;
+            if (sectionValue.Contains("$ServiceName"))
+                section.Value = sectionValue.Replace("$ServiceName", serviceInfo.ServiceName);
+
+            if (sectionValue.Contains("$ShortName"))
+                section.Value = sectionValue.Replace("$ShortName", serviceInfo.ShortName);
+        }
+    }
+
+    /// <summary>
+    /// Register Cofiguration ChangeCallback
+    /// </summary>
+    /// <param name="state"></param>
+    private static IDisposable _callbackRegistration;
+    private static IServiceInfo _serviceInfo;
+    private static void OnSettingConfigurationChanged(object state)
+    {
+        _callbackRegistration?.Dispose();
+        var configuration = state as IConfiguration;
+        var changedChildren = configuration.GetChildren();
+        var reloadToken = configuration.GetReloadToken();
+
+        ReplacePlaceholder(changedChildren, _serviceInfo);
+
+        _callbackRegistration = reloadToken.RegisterChangeCallback(OnSettingConfigurationChanged, state);
     }
 }
