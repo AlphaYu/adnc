2f076a6095d88c339945f8b6d49779bd4ad5dd3a
alpha.yu <xyz199809@gmail.com>
2022-06-14 19:11:08 +0800

    [feat]Program.cs代码优化调整

--- src/ServerApi/Services/Adnc.Ord/Adnc.Ord.WebApi/Program.cs
+++ src/ServerApi/Services/Adnc.Ord/Adnc.Ord.WebApi/Program.cs
@@ -8,22 +8,22 @@ internal static class Program
     internal static async Task Main(string[] args)
     {
         var webApiAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+        var serviceInfo = Shared.WebApi.ServiceInfo.CreateInstance(webApiAssembly);
         var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
         logger.Debug($"init {nameof(Program.Main)}");
         try
         {
-            //Configuration,ServiceCollection,Logging,WebHost(Kestrel)
-            var builder = WebApplication.CreateBuilder(args);
-            builder.ConfigureAdncDefault(args, webApiAssembly);
+            var app = WebApplication
+                .CreateBuilder(args)
+                .ConfigureAdncDefault(args, serviceInfo)
+                .Build();
 
-            var app = builder.Build();
-
-            //Middlewares
             app.UseAdncDefault();
-            app.UseRegistrationCenter();
 
-            //Start
-            await app.ChangeThreadPoolSettings().RunAsync();
+            await app
+                .ChangeThreadPoolSettings()
+                .UseRegistrationCenter()
+                .RunAsync();
         }
         catch (Exception ex)
         {
--- src/ServerApi/Services/Adnc.Usr/Adnc.Usr.WebApi/Program.cs
+++ src/ServerApi/Services/Adnc.Usr/Adnc.Usr.WebApi/Program.cs
@@ -8,15 +8,16 @@ internal static class Program
     internal static async Task Main(string[] args)
     {
         var webApiAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+        var serviceInfo = Shared.WebApi.ServiceInfo.CreateInstance(webApiAssembly);
         var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
         logger.Debug($"init {nameof(Program.Main)}");
         try
         {
             //Configuration,ServiceCollection,Logging,WebHost(Kestrel)
-            var builder = WebApplication.CreateBuilder(args);
-            builder.ConfigureAdncDefault(args, webApiAssembly);
-
-            var app = builder.Build();
+            var app = WebApplication
+                .CreateBuilder(args)
+                .ConfigureAdncDefault(args, serviceInfo)
+                .Build();
 
             //Middlewares
             app.UseAdncDefault(endpointRoute: endpoint =>
@@ -24,10 +25,12 @@ internal static class Program
                 endpoint.MapGrpcService<Grpc.AuthGrpcServer>();
                 endpoint.MapGrpcService<Grpc.UsrGrpcServer>();
             });
-            app.UseRegistrationCenter();
 
             //Start
-            await app.ChangeThreadPoolSettings().RunAsync();
+            await app
+                .ChangeThreadPoolSettings()
+                .UseRegistrationCenter()
+                .RunAsync();
         }
         catch (Exception ex)
         {
