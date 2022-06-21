2293241880a13280e86cec0af0b2b6b796b666e5
alpha.yu <xyz199809@gmail.com>
2022-06-21 12:39:27 +0800

    [feat]路径调整原来的usr/session=>auth/session

--- src/ClientApp/src/api/account.js
+++ src/ClientApp/src/api/account.js
@@ -2,31 +2,46 @@ import request from '@/utils/request'
 
 export function login(data) {
   return request({
-    url: '/usr/session',
+    url: '/auth/session',
     method: 'post',
     data
   })
 }
 
-export function getInfo() {
-  return request({
-    url: '/usr/session',
-    method: 'get'
-  })
-}
-
 export function logout(token) {
   return request({
-    url: '/usr/session',
+    url: '/auth/session',
     method: 'delete'
   })
 }
 
 export function updatePwd(params) {
   return request({
-    url: '/usr/session/password',
+    url: '/auth/session/password',
     method: 'put',
     data: params
+  })
+}
+
+export function RenewToken(data) {
+  return request({
+    url: '/auth/session',
+    method: 'put',
+    data
+  })
+}
+
+export function getAuthInfo() {
+  return request({
+    url: '/auth/session',
+    method: 'get'
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/usr/users/current',
+    method: 'get'
   })
 }
 
