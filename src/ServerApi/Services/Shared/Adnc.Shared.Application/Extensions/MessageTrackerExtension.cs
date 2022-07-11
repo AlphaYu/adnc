b553ae5b7c69f4602c52b697e623073ad62237fc
alpha.yu <xyz199809@gmail.com>
2022-07-11 23:19:25 +0800

    feat 新增MessageTracker扩展方法

---
+++ src/ServerApi/Services/Shared/Adnc.Shared.Application/Extensions/MessageTrackerExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace Adnc.Shared.Application.Contracts.Interfaces;
+
+public static class MessageTrackerExtension
+{
+    public static async Task<bool> HasProcessedAsync<T>(this IMessageTracker tracker, Events.EventEntity<T> eventDto)
+        where T : class
+    {
+        return await tracker.HasProcessedAsync(eventDto.Id, eventDto.EventTarget);
+    }
+
+    public static async Task MarkAsProcessedAsync<T>(this IMessageTracker tracker, Events.EventEntity<T> eventDto)
+    where T : class
+    {
+        await tracker.MarkAsProcessedAsync(eventDto.Id, eventDto.EventTarget);
+    }
+}
