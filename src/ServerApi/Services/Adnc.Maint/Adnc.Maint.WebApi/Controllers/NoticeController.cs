a8a0ee364ef252f1b09842f6a95aa0c5fd1e151b
alpha.yu <xyz199809@gmail.com>
2022-07-12 15:07:36 +0800

    feat NoticeController.GetList业务逻辑调整

--- src/ServerApi/Services/Adnc.Maint/Adnc.Maint.WebApi/Controllers/NoticeController.cs
+++ src/ServerApi/Services/Adnc.Maint/Adnc.Maint.WebApi/Controllers/NoticeController.cs
@@ -8,15 +8,28 @@
 public class NoticeController : AdncControllerBase
 {
     private readonly INoticeAppService _noticeService;
+    private readonly UserContext _userContext;
 
-    public NoticeController(INoticeAppService noticeService) => _noticeService = noticeService;
+    public NoticeController(
+        INoticeAppService noticeService,
+        UserContext userContext)
+    {
+        _noticeService = noticeService;
+        _userContext = userContext;
+    }
 
     /// <summary>
     /// 获取通知消息列表
     /// </summary>
     /// <param name="search"><see cref="NoticeSearchDto"/></param>
     /// <returns></returns>
+    [AllowAnonymous]
     [HttpGet()]
     public async Task<ActionResult<List<NoticeDto>>> GetList([FromQuery] NoticeSearchDto search)
-        => Result(await _noticeService.GetListAsync(search));
+    {
+        if (_userContext is null || _userContext.Id == 0)
+            return await Task.FromResult(new List<NoticeDto>());
+        else
+            return Result(await _noticeService.GetListAsync(search));
+    }
 }
