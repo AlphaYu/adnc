f3d08c31db203cbd01577b541933e9a097a47fca
alpha.yu <xyz199809@gmail.com>
2022-08-22 17:37:10 +0800

    refit 新增GetImplementationTypesWithOutAbstractClass，GetImplementationTypes两个扩展方法。

---
+++ src/ServerApi/Infrastructures/Adnc.Infra.Core/System/Extensions/Type/AssemblyExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace System;
+
+public static class AssemblyExtension
+{
+    public static IEnumerable<Type> GetImplementationTypesWithOutAbstractClass<TServiceType>(this Assembly assembly)
+        where TServiceType : class
+    {
+
+        var implTypes = GetImplementationTypes<TServiceType>(assembly).Where(type => type.IsNotAbstractClass(true));
+        return implTypes ?? Array.Empty<Type>();
+    }
+
+    public static IEnumerable<Type> GetImplementationTypes<TServiceType>(this Assembly assembly)
+    where TServiceType : class
+    {
+        var serviceType = typeof(TServiceType);
+        var implTypes = assembly.ExportedTypes.Where(type => type.IsAssignableTo(serviceType));
+        return implTypes ?? Array.Empty<Type>();
+    }
+}
+
