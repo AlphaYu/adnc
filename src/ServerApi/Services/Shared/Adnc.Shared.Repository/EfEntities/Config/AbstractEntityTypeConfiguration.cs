f866ca918c2932d837aff08790f84c1d0a0d563e
alpha.yu <xyz199809@gmail.com>
2022-08-23 15:08:57 +0800

    refactor 一部分类移到adnc.shared.repository工程，

---
+++ src/ServerApi/Services/Shared/Adnc.Shared.Repository/EfEntities/Config/AbstractEntityTypeConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace Adnc.Shared.Repository.EfEntities.Config;
+
+public abstract class AbstractEntityTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+   where TEntity : Entity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        var entityType = typeof(TEntity);
+        ConfigureKey(builder, entityType);
+        ConfigureConcurrency(builder, entityType);
+        ConfigureQueryFilter(builder, entityType);
+    }
+
+    protected virtual void ConfigureKey(EntityTypeBuilder<TEntity> builder, Type entityType)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id).HasColumnOrder(1).ValueGeneratedNever();
+    }
+
+    protected virtual void ConfigureConcurrency(EntityTypeBuilder<TEntity> builder, Type entityType)
+    {
+        if (typeof(IConcurrency).IsAssignableFrom(entityType))
+            builder.Property("RowVersion").IsRequired().IsRowVersion().ValueGeneratedOnAddOrUpdate();
+    }
+
+    protected virtual void ConfigureQueryFilter(EntityTypeBuilder<TEntity> builder, Type entityType)
+    {
+        if (typeof(ISoftDelete).IsAssignableFrom(entityType))
+        {
+            builder.Property("IsDeleted")
+                       .HasDefaultValue(false)
+                       .HasColumnOrder(2);
+            builder.HasQueryFilter(d => !EF.Property<bool>(d, "IsDeleted"));
+        }
+    }
+}
