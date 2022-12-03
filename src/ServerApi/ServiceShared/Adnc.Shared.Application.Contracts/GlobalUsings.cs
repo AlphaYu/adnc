4b2a4bc0102045b58191b306ffeff81344b3e5a8
alpha.yu <xyz199809@gmail.com>
2022-12-03 17:17:42 +0800

    feat 增加新的FluentValidation验证器

--- src/ServerApi/ServiceShared/Adnc.Shared.Application.Contracts/GlobalUsings.cs
+++ src/ServerApi/ServiceShared/Adnc.Shared.Application.Contracts/GlobalUsings.cs
@@ -1,4 +1,6 @@
 ﻿global using Adnc.Infra.Core.Json;
+global using FluentValidation;
+global using FluentValidation.Validators;
 global using System.Net;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
