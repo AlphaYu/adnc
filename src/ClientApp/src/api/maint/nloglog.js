a66d78c5c5b52e9c11c382149775ce8bb6f777bd
alpha <xyz199809@gmail.com>
2023-03-19 20:56:30 +0800

    refactor delete the clientApp directory and move the front-end code to the Adnc.Vue2 project.

--- src/ClientApp/src/api/maint/nloglog.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getList(params) {
-  return request({
-    url: '/maint/nloglogs',
-    method: 'get',
-    params
-  })
-}
--- src/ClientApp/src/api/maint/notice.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function getList(params) {
-  return request({
-    url: '/maint/notices',
-    method: 'get',
-    params
-  })
-}
