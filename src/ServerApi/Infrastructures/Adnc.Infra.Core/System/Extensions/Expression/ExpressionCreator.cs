e6af32d11f61d363e76e30d55ef4bc82f02ffa31
alpha <xyz199809@gmail.com>
2023-03-25 18:20:45 +0800

    chore add comments

--- src/ServerApi/Infrastructures/Adnc.Infra.Core/System/Extensions/Expression/ExpressionCreator.cs
+++ src/ServerApi/Infrastructures/Adnc.Infra.Core/System/Extensions/Expression/ExpressionCreator.cs
@@ -1,14 +1,30 @@
 ﻿namespace System.Linq.Expressions
 {
+    /// <summary>
+    /// Expression creator
+    /// </summary>
     public static class ExpressionCreator
     {
+        /// <summary>
+        /// Generate a new expression
+        /// </summary>
+        /// <typeparam name="T">The type of the object being tested.</typeparam>
+        /// <param name="expr">The expression.</param>
+        /// <returns>The combined expression.</returns>
         public static Expression<Func<T, bool>> New<T>(Expression<Func<T, bool>>? expr = null)
+            where T : class
             => expr ?? (x => true);
 
+        /// <summary>
+        /// Generate a new expression
+        /// </summary>
+        /// <typeparam name="T1">The type of the first object being tested.</typeparam>
+        /// <typeparam name="T2">The type of the second object being tested.</typeparam>
+        /// <param name="expr">The expression.</param>
+        /// <returns>The combined expression.</returns>
         public static Expression<Func<T1, T2, bool>> New<T1, T2>(Expression<Func<T1, T2, bool>>? expr = null)
+            where T1 : class
+            where T2 : class
             => expr ?? ((x, y) => true);
-
-        public static Expression<Func<T1, T2, T3, bool>> New<T1, T2, T3>(Expression<Func<T1, T2, T3, bool>>? expr = null)
-            => expr ?? ((x, y, z) => true);
     }
 }
