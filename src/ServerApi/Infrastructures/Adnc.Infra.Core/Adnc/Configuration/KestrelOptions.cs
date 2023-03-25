4a2755a4b72ee182e0662500250cb66961a0fafe
alpha <xyz199809@gmail.com>
2023-03-25 22:18:57 +0800

    chore add comments

--- src/ServerApi/Infrastructures/Adnc.Infra.Core/Adnc/Configuration/KestrelOptions.cs
+++ src/ServerApi/Infrastructures/Adnc.Infra.Core/Adnc/Configuration/KestrelOptions.cs
@@ -1,21 +1,37 @@
 ﻿namespace Adnc.Infra.Core.Configuration;
 
+/// <summary>
+/// Represents options for Kestrel server.
+/// </summary>
 public class KestrelOptions
 {
-    public IDictionary<string, Endpoint> Endpoints { get; set; }
+    /// <summary>
+    /// Gets or sets the endpoints for the Kestrel server.
+    /// </summary>
+    public IDictionary<string, Endpoint> Endpoints { get; init; } = new Dictionary<string, Endpoint>();
 
-    public KestrelOptions() => Endpoints = new Dictionary<string, Endpoint>();
-
+    /// <summary>
+    /// Represents an endpoint for the Kestrel server.
+    /// </summary>
     public class Endpoint
     {
-        public Endpoint()
-        {
-            if (string.IsNullOrWhiteSpace(Protocols))
-                Protocols = "Http1AndHttp2";
-        }
+        private const string DefaultProtocols = "Http1AndHttp2";
 
+        /// <summary>
+        /// Gets or sets the URL for the endpoint.
+        /// </summary>
         public string Url { get; set; } = string.Empty;
 
-        public string Protocols { get; set; }
+        /// <summary>
+        /// Gets or sets the supported protocols for the endpoint.
+        /// </summary>
+        public string Protocols { get; set; } = DefaultProtocols;
+
+        /// <summary>
+        /// Initializes a new instance of the Endpoint class.
+        /// </summary>
+        public Endpoint()
+        {
+        }
     }
 }
