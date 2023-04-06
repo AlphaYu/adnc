1abfadb8642897ed926ce004ada45e2289c3e358
alpha <xyz199809@gmail.com>
2023-04-06 22:45:12 +0800

    feat add extension method GetMethodName

--- src/ServerApi/Infrastructures/Adnc.Infra.Core/System/Extensions/Type/MethodInfoExtension.cs
+++ src/ServerApi/Infrastructures/Adnc.Infra.Core/System/Extensions/Type/MethodInfoExtension.cs
@@ -1,12 +1,24 @@
-﻿namespace System.Reflection;
+﻿using System.Text.RegularExpressions;
+
+namespace System.Reflection;
 
 public static class MethodInfoExtension
 {
-    public static bool IsAsyncMethod(this MethodInfo @this)
+    public static bool IsAsyncMethod(this MethodInfo methodInfo)
     {
         return (
-            @this.ReturnType == typeof(Task)
-            || (@this.ReturnType.IsGenericType && @this.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
+            methodInfo.ReturnType == typeof(Task)
+            || (methodInfo.ReturnType.IsGenericType && methodInfo.ReturnType.GetGenericTypeDefinition() == typeof(Task<>))
         );
+    }
+   
+    public static string GetMethodName(this MethodBase methodInfo)
+    {
+        var originalMethodName = methodInfo.DeclaringType?.Name ?? string.Empty;
+        if (string.IsNullOrWhiteSpace(originalMethodName))
+            return string.Empty;
+
+        var match = Regex.Match(originalMethodName, "<(.*)>");
+        return match.Success ? match.Groups[1].Value : originalMethodName;
     }
 }
