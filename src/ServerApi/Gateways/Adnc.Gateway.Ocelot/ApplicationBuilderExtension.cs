39292d3b5f667ab52340d5cb8ff012105457bd81
Alpha Yu <xyz199809@gmail.com>
2023-04-25 21:14:57 +0800

    feat add src/ServerApi/Gateways/Adnc.Gateway.Ocelot/wwwroot/adnc-topics.png

--- src/ServerApi/Gateways/Adnc.Gateway.Ocelot/ApplicationBuilderExtension.cs
+++ src/ServerApi/Gateways/Adnc.Gateway.Ocelot/ApplicationBuilderExtension.cs
@@ -9,22 +9,26 @@ namespace Adnc.Gateway.Ocelot
             var configration = app.ApplicationServices.GetRequiredService<IConfiguration>();
             var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
 
-            var outputHtml = new StringBuilder($"Talk is cheap. Show me the code.", 500);
-            outputHtml.AppendLine($"<br>ASPNETCORE_ENVIRONMENT={env.EnvironmentName}<br>");
+            var outputHtml = new StringBuilder(500);
+            outputHtml.Append("<div align='center'>");
+            outputHtml.Append(@"<a href='https://github.com/alphayu/adnc' target='_blank'><img src='adnc-topics.png'/></a>");
+            outputHtml.Append($"<br><br>ASPNETCORE_ENVIRONMENT={env.EnvironmentName}<br>");
             var serviceRoters = configration.GetSection("Routes").Get<List<ServiceRouter>>();
             if (serviceRoters is not null)
             {
                 var routerDictionaryies = serviceRoters.Where(x => !x.Path.StartsWith("/auth")).GroupBy(x => x.Group);
                 foreach (var group in routerDictionaryies)
                 {
-                    outputHtml.AppendLine($"<br>{group.Key}");
-                    outputHtml.AppendLine($"<br>---------------------------------------------------------<br>");
+                    outputHtml.Append($"<br><b>{group.Key}</b>");
+                    outputHtml.Append($"<br>--------------------------------------------------------------------------------------------<br>");
                     foreach (var router in group)
                     {
-                        outputHtml.AppendLine($"<a href='{router.Path}' target='_blank'>{router.ServiceName}</a>&nbsp;&nbsp;");
+                        outputHtml.Append($"<a href='{router.Path}' target='_blank'>{router.ServiceName}</a>&nbsp;&nbsp;");
                     }
                 }
             }
+            outputHtml.Append($"<br><br>{DateTime.Now}");
+            outputHtml.Append("</div>");
             return outputHtml.ToString();
         }
     }
