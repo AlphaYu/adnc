47a52fbbb5b931f1282c4792b0178fd5827a99cd
Alpha Yu <xyz199809@gmail.com>
2025-02-19 09:30:43 +0800

    feat:ServiceCollectionExtensions.Configuration.cs，ServiceCollectionExtensions.Internal.cs 合并到 ServiceCollectionExtensions.cs文件

---
+++ src/ServerApi/Infrastructures/Adnc.Infra.Core/Microsoft/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class ServiceCollectionExtensions
+{
+    private static readonly ConcurrentDictionary<string, char> s_RegisteredModels = new();
+
+    public static bool HasRegistered(this IServiceCollection _, string modelName) => !s_RegisteredModels.TryAdd(modelName.ToLower(), '1');
+
+    public static IServiceCollection ReplaceConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services.Replace(ServiceDescriptor.Singleton(configuration));
+    }
+
+    public static IConfiguration GetConfiguration(this IServiceCollection services)
+    {
+        var hostBuilderContext = services.GetSingletonInstanceOrNull<HostBuilderContext>();
+        if (hostBuilderContext?.Configuration is not null)
+        {
+            var instance = hostBuilderContext.Configuration as IConfigurationRoot;
+            if (instance is not null)
+                return instance;
+        }
+
+        return services.GetSingletonInstance<IConfiguration>();
+    }
+
+    internal static T? GetSingletonInstanceOrNull<T>(this IServiceCollection services)
+        where T : class
+    {
+        var instance = services.FirstOrDefault(d => d.ServiceType == typeof(T))?.ImplementationInstance;
+        if (instance is null)
+            return null;
+
+        return (T)instance;
+    }
+
+    internal static T GetSingletonInstance<T>(this IServiceCollection services)
+        where T : class
+    {
+        var instance = GetSingletonInstanceOrNull<T>(services);
+        if (instance is null)
+            throw new InvalidOperationException("Could not find singleton service: " + typeof(T).AssemblyQualifiedName);
+        return instance;
+    }
+}
