c54da71afdd1cc13c7fa8f9565e82737c930e5d8
Alpha Yu <xyz199809@gmail.com>
2025-02-19 21:19:07 +0800

    feat:调整savechange方法，因为efcore8增加了AutoTransactionBehavior = whenneed，可以替代现有方案。

--- src/ServerApi/Infrastructures/Adnc.Infra.EfCore/AdncDbContext.cs
+++ src/ServerApi/Infrastructures/Adnc.Infra.EfCore/AdncDbContext.cs
@@ -9,29 +9,17 @@ public abstract class AdncDbContext : DbContext
         : base(options)
     {
         _entityInfo = entityInfo;
-        Database.AutoTransactionsEnabled = false;
         _operater = operater;
+        Database.AutoTransactionBehavior = AutoTransactionBehavior.WhenNeeded;
         //ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
     }
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
+        //efcore7 support this feature
+        //Database.AutoTransactionBehavior = AutoTransactionBehavior.WhenNeeded;
         var changedEntities = SetAuditFields();
-
-        //没有自动开启事务的情况下,保证主从表插入，主从表更新开启事务。
-        var isManualTransaction = false;
-        if (!Database.AutoTransactionsEnabled && Database.CurrentTransaction is null && changedEntities > 1)
-        {
-            isManualTransaction = true;
-            Database.AutoTransactionsEnabled = true;
-        }
-
         var result = base.SaveChangesAsync(cancellationToken);
-
-        //如果手工开启了自动事务，用完后关闭。
-        if (isManualTransaction)
-            Database.AutoTransactionsEnabled = false;
-
         return result;
     }
 
