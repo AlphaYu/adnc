94b4c6b5daa01483098a6b3fe5b804393536b0ad
Alpha Yu <xyz199809@gmail.com>
2025-03-13 00:14:32 +0800

    refactor:文件目录调整

---
+++ src/Infrastructures/Core/Json/BoolConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace System.Text.Json;
+
+public class BoolConverter : JsonConverter<bool>
+{
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var obj = reader.GetInt32();
+        return obj == 0 ? false : true;
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+    {
+        writer.WriteNumberValue(value ? 1 : 0);
+    }
+}
