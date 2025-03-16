69211543e1b8fce1a3faeef0787f3df9447edc2d
Alpha Yu <xyz199809@gmail.com>
2025-03-16 22:29:07 +0800

    refactor:重构认证相关类和接口
    - 更新 `LoginResponse.cs` 文件，修改命名空间和类名，以更好地反映用途。
    - 新增 `UserValidatedInfoResponse` 记录类型，包含用户验证状态和版本信息。
    - 删除不再使用的 `LoginInputRto` 和 `UserValidatedInfoRto` 类。
    - 更新 `IAuthRestClient` 接口，调整方法参数和返回类型，使用新的响应类型。

--- src/ServiceShared/Remote/Http/Messages/LoginResponse.cs
+++ src/ServiceShared/Remote/Http/Messages/LoginResponse.cs
@@ -1,12 +1,12 @@
-﻿namespace Adnc.Shared.Remote.Http.Rtos;
+﻿namespace Adnc.Shared.Remote.Http.Messages;
 
-public class LoginRto
+public class LoginResponse
 {
     /// <summary>
     /// 构造函数
     /// 修复Warning, add by garfield 20220530
     /// </summary>
-    public LoginRto(string token, string refreshToken) => (Token, RefreshToken) = (token, refreshToken);
+    public LoginResponse(string token, string refreshToken) => (Token, RefreshToken) = (token, refreshToken);
 
     public string Token { get; set; }
 
