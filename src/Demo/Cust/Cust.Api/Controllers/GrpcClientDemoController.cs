0671eafde66c41d1436491f7379920c1540f039b
Alpha Yu <xyz199809@gmail.com>
2025-03-17 15:29:44 +0800

    feat:添加 gRPC 和 REST 客户端示例控制器
    在 `GrpcClientDemoController.cs` 中实现了 gRPC 客户端，添加了获取字典数据选项列表和系统配置列表的异步方法。
    
    在 `RestClientDemoController.cs` 中实现了 REST 客户端，添加了获取字典数据选项列表和系统配置列表的异步方法。
    
    两个控制器的路由分别设置为 `/grpc` 和 `/rest`，并标记为 `ApiController`。

---
+++ src/Demo/Cust/Cust.Api/Controllers/GrpcClientDemoController.cs
@@ -0,0 +1,45 @@
+﻿using Adnc.Demo.Remote.Grpc;
+using Adnc.Demo.Remote.Grpc.Messages;
+using Adnc.Demo.Remote.Grpc.Services;
+
+namespace Adnc.Demo.Cust.Api.Controllers;
+
+/// <summary>
+/// gRPC demo
+/// </summary>
+[Route($"{RouteConsts.CustRoot}/grpc")]
+[ApiController]
+public class GrpcClientDemoController(AdminGrpc.AdminGrpcClient adminGrpcClient) : AdncControllerBase
+{
+    /// <summary>
+    /// 获取字典数据选项列表
+    /// </summary>
+    /// <param name="codes"></param>
+    /// <returns></returns>
+    [AllowAnonymous, HttpGet()]
+    [Route("dictoptions")]
+    public async Task<ActionResult<List<DictOptionReply>>> GetDictOptionsAsync(string codes = "all")
+    {
+        var request = new DictOptionRequest { Codes = codes };
+        var grpcResult = await adminGrpcClient.GetDictOptionsAsync(request, GrpcClientConsts.BasicHeader);
+        if (grpcResult is not null)
+            return grpcResult.List.ToList();
+        return NoContent();
+    }
+
+    /// <summary>
+    /// 获取系统配置列表
+    /// </summary>
+    /// <param name="keys"></param>
+    /// <returns></returns>
+    [AllowAnonymous, HttpGet()]
+    [Route("sysconfigs")]
+    public async Task<ActionResult<List<SysConfigSimpleReply>>> GetSysConfigListAsync(string keys = "all")
+    {
+        var request = new SysConfigSimpleRequest { Keys = keys };
+        var grpcResult =await adminGrpcClient.GetSysConfigListAsync(request, GrpcClientConsts.BasicHeader);
+        if (grpcResult is not null)
+            return grpcResult.List.ToList();
+        return NoContent();
+    }
+}
---
+++ src/Demo/Cust/Cust.Api/Controllers/RestClientDemoController.cs
@@ -0,0 +1,42 @@
+﻿using Adnc.Demo.Remote.Http.Messages;
+using Adnc.Demo.Remote.Http.Services;
+
+namespace Adnc.Demo.Cust.Api.Controllers;
+
+/// <summary>
+/// REST  demo
+/// </summary>
+[Route($"{RouteConsts.CustRoot}/rest")]
+[ApiController]
+public class RestClientDemoController(IAdminRestClient adminRestClient) : AdncControllerBase
+{
+    /// <summary>
+    /// 获取字典数据选项列表
+    /// </summary>
+    /// <param name="codes"></param>
+    /// <returns></returns>
+    [AllowAnonymous,HttpGet()]
+    [Route("dictoptions")]
+    public async Task<ActionResult<List<DictOptionResponse>>> GetDictOptionsAsync(string codes = "all")
+    {
+        var restResult = await adminRestClient.GetDictOptionsAsync(codes);
+        if (restResult.IsSuccessStatusCode && restResult.Content is not null)
+            return restResult.Content;
+        return NoContent();
+    }
+
+    /// <summary>
+    /// 获取系统配置列表
+    /// </summary>
+    /// <param name="keys"></param>
+    /// <returns></returns>
+    [AllowAnonymous, HttpGet()]
+    [Route("sysconfigs")]
+    public async Task<ActionResult<List<SysConfigSimpleResponse>>> GetSysConfigListAsync(string keys = "all")
+    {
+        var restResult = await adminRestClient.GetSysConfigListAsync(keys);
+        if (restResult.IsSuccessStatusCode && restResult.Content is not null)
+            return restResult.Content;
+        return NoContent();
+    }
+}
