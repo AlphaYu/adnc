2d01264217dc7ccf3988b74eaa4bafce2343a7bc
Alpha Yu <xyz199809@gmail.com>
2025-03-18 22:40:38 +0800

    refactor:重构 CapPublisher 类并更新 IEventPublisher 接口
    在 `CapPublisher.cs` 中，修改了构造函数和方法签名，使用 `ICapPublisher publisher` 替代 `_eventBus`。简化了多个 `PublishAsync` 和 `Publish` 方法的实现，并新增了 `PublishDelay` 方法以支持延迟发布功能。
    
    在 `IEventPublisher.cs` 接口中，更新了方法签名以保持一致性，并新增了 `PublishDelayAsync` 和 `PublishDelay` 方法的声明。

--- src/Infrastructures/EventBus/IEventPublisher.cs
+++ src/Infrastructures/EventBus/IEventPublisher.cs
@@ -2,12 +2,20 @@
 {
     public interface IEventPublisher
     {
-        public Task PublishAsync<T>(T eventObj, string? callbackName = null, CancellationToken cancellationToken = default) where T : class;
+        Task PublishAsync<T>(T contentObj, string? callbackName = null, CancellationToken cancellationToken = default) where T : class;
 
-        public Task PublishAsync<T>(T eventObj, IDictionary<string, string?> headers, CancellationToken cancellationToken = default) where T : class;
+        Task PublishAsync<T>(T contentObj, IDictionary<string, string?> headers, CancellationToken cancellationToken = default) where T : class;
 
-        public void Publish<T>(T eventObj, string? callbackName = null) where T : class;
+        void Publish<T>(T contentObj, string? callbackName = null) where T : class;
 
-        public void Publish<T>(T eventObj, IDictionary<string, string?> headers) where T : class;
+        void Publish<T>(T contentObj, IDictionary<string, string?> headers) where T : class;
+
+        Task PublishDelayAsync<T>(TimeSpan delayTime, T? contentObj, IDictionary<string, string?> headers, CancellationToken cancellationToken = default);
+
+        Task PublishDelayAsync<T>(TimeSpan delayTime, T? contentObj, string? callbackName = null, CancellationToken cancellationToken = default);
+
+        void PublishDelay<T>(TimeSpan delayTime, T? contentObj, IDictionary<string, string?> headers);
+
+        void PublishDelay<T>(TimeSpan delayTime, T? contentObj, string? callbackName = null);
     }
 }
