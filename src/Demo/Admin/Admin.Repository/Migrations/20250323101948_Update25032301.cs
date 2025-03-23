46bbfe1eb3c17aff229b8fa999e049e87a30e857
Alpha Yu <xyz199809@gmail.com>
2025-03-23 20:55:27 +0800

    chore:更新数据库迁移文件，添加新实体定义

---
+++ src/Demo/Admin/Admin.Repository/Migrations/20250323101948_Update25032301.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adnc.Demo.Admin.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update25032301 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/Demo/Maint/Maint.Repository/Migrations/20250323125304_Update25032302.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adnc.Demo.Maint.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update25032302 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
