b4a38d59625b1a7ec867e5631712d374a9e2ff08
Alpha Yu <xyz199809@gmail.com>
2025-03-25 10:43:37 +0800

    refactor:完善CustomExceptionHandlerMiddleware 类，增加DEMO_SERVER 环境变量处理。
    对构造函数进行修改，增强异常处理逻辑，增加对环境变量 `DEMO_SERVER` 的检查以控制 HTTP 方法请求。对于不允许的请求，返回 403 状态码和相应的错误信息。旧代码已注释，更新了问题详情格式化逻辑以适应新的处理方式。

--- src/ServiceShared/WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
+++ src/ServiceShared/WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -2,81 +2,67 @@
 
 namespace Adnc.Shared.WebApi.Middleware;
 
-public class CustomExceptionHandlerMiddleware
+public class CustomExceptionHandlerMiddleware(RequestDelegate next, IWebHostEnvironment env, ILogger<CustomExceptionHandlerMiddleware> logger)
 {
-    private readonly ILogger<CustomExceptionHandlerMiddleware> _logger;
-    private readonly IWebHostEnvironment _env;
-    private readonly RequestDelegate _next;
-
-    public CustomExceptionHandlerMiddleware(RequestDelegate next
-        , IWebHostEnvironment env
-        , ILogger<CustomExceptionHandlerMiddleware> logger)
-    {
-        _next = next;
-        _env = env;
-        _logger = logger;
-    }
-
     public async Task Invoke(HttpContext context)
     {
         try
         {
-            await _next(context);
+            string envVariable = (Environment.GetEnvironmentVariable("DEMO_SERVER") ?? "false").ToLower();
+            if (envVariable == "false")
+            {
+                await next(context);
+            }
+            else
+            {
+                string[] methods = ["post", "put", "delete", "patch"];
+                string curretMethod = context.Request.Method.ToLower();
+                if (!methods.Contains(curretMethod))
+                {
+                    await next(context);
+                }
+                else
+                {
+                    string? path = context.Request.Path.Value;
+                    if (path is null || path.Contains("auth/session", StringComparison.CurrentCultureIgnoreCase)
+                        || path.Contains("profiler/results", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        await next(context);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 403;
+                        context.Response.ContentType = "application/problem+json";
+                        var type = string.Concat("https://httpstatuses.com/", context.Response.StatusCode);
+                        var title = "演示环境";
+                        var detial = "演示环境,不允许操作";
+                        var problemDetails = new ProblemDetails { Title = title, Detail = detial, Type = type, Status = context.Response.StatusCode };
+                        var errorText = JsonSerializer.Serialize(problemDetails, SystemTextJson.GetAdncDefaultOptions());
+                        await context.Response.WriteAsync(errorText);
+                    }
+                }
+            }
         }
         catch (Exception ex)
         {
             await HandleExceptionAsync(context, ex);
         }
-
-        #region old code
-
-        //var statusCode = context.Response.StatusCode;
-        //string msg = string.Empty;
-        //if (statusCode == 401)
-        //{
-        //    msg = "未授权";
-        //}
-        //else if (statusCode == 403)
-        //{
-        //    msg = "未授权，没有权限";
-        //}
-        //else if (statusCode == 404)
-        //{
-        //    msg = "未找到服务";
-        //}
-
-        //if (!string.IsNullOrWhiteSpace(msg))
-        //{
-        //    await HandleExceptionAsync(context, statusCode, msg);
-        //}
-
-        #endregion old code
     }
 
     private async Task HandleExceptionAsync(HttpContext context, Exception exception)
     {
-        var requestId = System.Diagnostics.Activity.Current?.Id ?? context.TraceIdentifier; 
-        var eventId = new EventId( exception.HResult, requestId);
-        _logger.LogError(eventId, exception, exception.Message);
+        var requestId = System.Diagnostics.Activity.Current?.Id ?? context.TraceIdentifier;
+        var eventId = new EventId(exception.HResult, requestId);
+        logger.LogError(eventId, exception, exception.Message);
 
         var status = 500;
         var type = string.Concat("https://httpstatuses.com/", status);
-        var title = _env.IsDevelopment() ? exception.Message : $"系统异常";
-        var detial = _env.IsDevelopment() ? exception.GetExceptionDetail() : $"系统异常,请联系管理员({eventId})";
-
-        var problemDetails = new ProblemDetails
-        {
-            Title = title
-            ,
-            Detail = detial
-            ,
-            Type = type
-            ,
-            Status = status
-        };
+        var title = env.IsDevelopment() ? exception.Message : $"系统异常";
+        var detial = env.IsDevelopment() ? exception.GetExceptionDetail() : $"系统异常,请联系管理员({eventId})";
 
         context.Response.StatusCode = status;
         context.Response.ContentType = "application/problem+json";
+        var problemDetails = new ProblemDetails { Title = title, Detail = detial, Type = type, Status = status };
         var errorText = JsonSerializer.Serialize(problemDetails, SystemTextJson.GetAdncDefaultOptions());
         await context.Response.WriteAsync(errorText);
     }
