01e7b7319dcf699f7eb8ffca45b4c363844b03ad
Alpha Yu <xyz199809@gmail.com>
2025-03-25 22:10:13 +0800

    refactor:更新 ServiceLocator 类以支持新方法
    在 `ServiceLocator` 类中，添加了过时的属性标记，建议使用 `GetProvider` 方法来获取当前的 `IServiceProvider` 实例。新增了 `GetProvider` 方法和 `HasProvider` 方法，以便于获取当前的服务提供者实例和检查服务提供者是否已设置。

--- src/Infrastructures/Core/DependencyInjection/ServiceLocator.cs
+++ src/Infrastructures/Core/DependencyInjection/ServiceLocator.cs
@@ -18,7 +18,28 @@ public static class ServiceLocator
     /// <exception cref="ArgumentNullException">Thrown if value is null when setting Provider.</exception>
     public static IServiceProvider? Provider
     {
+        [Obsolete($"use {nameof(GetProvider)} instead")]
         get => _provider;
         set => _provider = _provider is not null ? throw new InvalidOperationException($"{nameof(Provider)} is already set.") : value ?? throw new ArgumentNullException(nameof(value));
     }
+
+    /// <summary>
+    /// Gets the current IServiceProvider instance
+    /// </summary>
+    /// <returns>The current IServiceProvider instance</returns>
+    /// <exception cref="InvalidOperationException">Thrown if Provider is not set.</exception>
+    public static IServiceProvider GetProvider()
+    {
+        if (_provider is null)
+            throw new InvalidOperationException($"{nameof(Provider)} is not set.");
+
+        return _provider;
+    }
+
+    /// <summary>
+    /// Checks if the IServiceProvider instance is set
+    /// </summary>
+    /// <returns>True if the IServiceProvider instance is set; otherwise, false.</returns>
+    public static bool HasProvider()
+        => _provider is not null;
 }
