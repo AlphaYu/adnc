831360ded10dacc77b1fe6df22d43d37bfe9d6aa
Alpha Yu <xyz199809@gmail.com>
2025-03-25 22:12:15 +0800

    perf:简化 AggregateRoot.cs 中的代码
    将 RowVersion 属性的初始化值更改为简写语法 `[]`，并调整获取 IEventPublisher 的方式，使用 ServiceLocator.GetProvider() 替代 ServiceLocator.Provider，以提高代码的可读性和简洁性。

--- src/ServiceShared/Domain/Entities/AggregateRoot.cs
+++ src/ServiceShared/Domain/Entities/AggregateRoot.cs
@@ -2,15 +2,10 @@
 
 public abstract class AggregateRoot : DomainEntity, IConcurrency, IEfEntity<long>
 {
-    public byte[] RowVersion { get; set; } = Array.Empty <byte>();
+    public byte[] RowVersion { get; set; } = [];
 
     public Lazy<IEventPublisher> EventPublisher => new(() =>
     {
-        //var httpContext = InfraHelper.Accessor.GetCurrentHttpContext();
-        //if (httpContext is not null)
-        //    return httpContext.RequestServices.GetRequiredService<IEventPublisher>();
-        if (ServiceLocator.Provider is not null)
-            return ServiceLocator.Provider.GetRequiredService<IEventPublisher>();
-        throw new NotImplementedException(nameof(IEventPublisher));
+        return ServiceLocator.GetProvider().GetRequiredService<IEventPublisher>();
     });
 }
