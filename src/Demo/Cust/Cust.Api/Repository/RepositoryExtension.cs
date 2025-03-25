4575144cd8993784bb8f72501a8fcd6239f96d6a
Alpha Yu <xyz199809@gmail.com>
2025-03-25 22:13:41 +0800

    chore:简化对 AdoQuerier 的访问方式
    将对 `AdoQuerier` 的访问从局部变量 `adoQuerier` 改为直接通过 `repository.AdoQuerier` 进行访问。这一更改确保了在调用 `QuerySingleAsync` 和 `QueryAsync` 方法时，始终使用 `repository` 中的 `AdoQuerier`，从而简化了代码并提高了可读性。

--- src/Demo/Cust/Cust.Api/Repository/RepositoryExtension.cs
+++ src/Demo/Cust/Cust.Api/Repository/RepositoryExtension.cs
@@ -15,14 +15,13 @@
         public static async Task<QueryPageResult<TResult>> GetPagedCustmersBySqlAsync<TResult>(this IEfRepository<Customer> repository, QueryCondition condition, int offset, int rows)
             where TResult : notnull
         {
-            var adoQuerier = repository.AdoQuerier ?? throw new NullReferenceException("AdoQuerier is null");
             string countSql = $"select count(*) from cust_customer {condition.Where}";
-            var total = await adoQuerier.QuerySingleAsync<int>(countSql, condition.Param);
+            var total = await repository.AdoQuerier.QuerySingleAsync<int>(countSql, condition.Param);
             if (total == 0)
                 return new QueryPageResult<TResult>(total);
 
             string sql = $"select * from cust_customer  {condition.Where} {condition.OrderBy} limit {offset},{rows}";
-            var result = await adoQuerier.QueryAsync<TResult>(sql, condition.Param);
+            var result = await repository.AdoQuerier.QueryAsync<TResult>(sql, condition.Param);
             return new QueryPageResult<TResult>(total, result);
         }
     }
