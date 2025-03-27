af1ed1080f273ca350718914eae4ed556e213327
Alpha Yu <xyz199809@gmail.com>
2025-03-27 21:31:45 +0800

    chore:简化 AdncDbContext 构造函数和方法
    移除了 `AdncDbContext` 类中的私有字段 `_entityInfo` 和 `_operater`，直接使用构造函数参数 `entityInfo` 和 `operater`。在 `OnModelCreating` 和 `SetAuditFields` 方法中相应地进行了更新，以提高代码的简洁性和可读性。

--- src/Infrastructures/Repository.EfCore/AdncDbContext.cs
+++ src/Infrastructures/Repository.EfCore/AdncDbContext.cs
@@ -2,9 +2,6 @@
 
 public abstract class AdncDbContext(DbContextOptions options, IEntityInfo entityInfo, Operater operater) : DbContext(options)
 {
-    private readonly IEntityInfo _entityInfo = entityInfo;
-    private readonly Operater _operater = operater;
-
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
         //efcore7 support this feature
@@ -22,7 +19,7 @@ public abstract class AdncDbContext(DbContextOptions options, IEntityInfo entity
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        _entityInfo.OnModelCreating(modelBuilder);
+        entityInfo.OnModelCreating(modelBuilder);
     }
 
     protected virtual int SetAuditFields()
@@ -30,14 +27,14 @@ public abstract class AdncDbContext(DbContextOptions options, IEntityInfo entity
         var allBasicAuditEntities = ChangeTracker.Entries<IBasicAuditInfo>().Where(x => x.State == EntityState.Added);
         allBasicAuditEntities.ForEach(entry =>
         {
-            entry.Entity.CreateBy = _operater.Id;
+            entry.Entity.CreateBy = operater.Id;
             entry.Entity.CreateTime = DateTime.Now;
         });
 
         var auditFullEntities = ChangeTracker.Entries<IFullAuditInfo>().Where(x => x.State == EntityState.Modified || x.State == EntityState.Added);
         auditFullEntities.ForEach(entry =>
         {
-            entry.Entity.ModifyBy = _operater.Id;
+            entry.Entity.ModifyBy = operater.Id;
             entry.Entity.ModifyTime = DateTime.Now;
         });
 
