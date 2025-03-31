66cf8aace771af5a888a337d8ac08f22ad5eb894
Alpha Yu <xyz199809@gmail.com>
2025-03-31 21:43:37 +0800

    feat: Update service registration logic
    - Modified `AddAdncInfraEfCoreMySql` method in `ServiceCollectionExtension.cs` to add `Assembly assembly` and `ServiceLifetime serviceLifetime` parameters.
    - Added null checks for parameters to ensure validity.
    - Changed service registration from `TryAddScoped` to `ServiceDescriptor`.
    - Added service registration for `IEntityInfo` type.

--- src/Infrastructures/Repository.EfCore.Mysql/Extensions/ServiceCollectionExtension.cs
+++ src/Infrastructures/Repository.EfCore.Mysql/Extensions/ServiceCollectionExtension.cs
@@ -1,21 +1,33 @@
-﻿using Adnc.Infra.Repository.EfCore.MySql;
+﻿using System.Reflection;
+using Adnc.Infra.Repository.EfCore.MySql;
 using Adnc.Infra.Repository.EfCore.MySql.Transaction;
+using Adnc.Infra.Repository.Interceptor.Castle;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtension
 {
-    public static IServiceCollection AddAdncInfraEfCoreMySql(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsBuilder)
+    public static IServiceCollection AddAdncInfraEfCoreMySql(this IServiceCollection services, Assembly assembly, Action<DbContextOptionsBuilder> optionsBuilder, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
     {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+        ArgumentNullException.ThrowIfNull(assembly, nameof(assembly));
+        ArgumentNullException.ThrowIfNull(optionsBuilder, nameof(optionsBuilder));
+
         if (services.HasRegistered(nameof(AddAdncInfraEfCoreMySql)))
         {
             return services;
         }
 
-        services.TryAddScoped<IUnitOfWork, MySqlUnitOfWork<MySqlDbContext>>();
-        services.TryAddScoped(typeof(IEfRepository<>), typeof(EfRepository<>));
-        services.TryAddScoped(typeof(IEfBasicRepository<>), typeof(EfBasicRepository<>));
-        services.AddDbContext<DbContext, MySqlDbContext>(optionsBuilder);
+        services.AddDbContext<DbContext, MySqlDbContext>(optionsBuilder, serviceLifetime);
+        services.Add(new ServiceDescriptor(typeof(IUnitOfWork), typeof(MySqlUnitOfWork<MySqlDbContext>), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(UowInterceptor), typeof(UowInterceptor), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(UowAsyncInterceptor), typeof(UowAsyncInterceptor), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(IEfRepository<>), typeof(EfRepository<>), serviceLifetime));
+        services.Add(new ServiceDescriptor(typeof(IEfBasicRepository<>), typeof(EfBasicRepository<>), serviceLifetime));
+
+        var serviceType = typeof(IEntityInfo);
+        var implType = assembly.ExportedTypes.Single(type => type.IsAssignableTo(serviceType) && type.IsNotAbstractClass(true));
+        services.Add(new ServiceDescriptor(serviceType, implType, serviceLifetime));
 
         return services;
     }
