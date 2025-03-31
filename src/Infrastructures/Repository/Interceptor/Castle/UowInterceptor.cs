672e0a8e235c272f868107bb5e7be22f0fdbb8b5
Alpha Yu <xyz199809@gmail.com>
2025-03-31 21:47:34 +0800

    refactor: Refactor transaction handling classes and project files
    - Removed `Entities\EfEntities\Extensions\**` reference from `Adnc.Infra.Repository.csproj` and added `Castle.Core.AsyncInterceptor` package.
    - Deleted `PocoLoadingExtension.cs` file.
    - Added `UowAsyncInterceptor` implementing `IAsyncInterceptor` to handle sync/async interception and transaction logic.
    - Introduced `UowInterceptor` implementing `IInterceptor`, utilizing `UowAsyncInterceptor` for interception.
    - Created `UnitOfWorkAttribute` to mark methods requiring transaction handling, including the `Distributed` property.

---
+++ src/Infrastructures/Repository/Interceptor/Castle/UowInterceptor.cs
@@ -0,0 +1,14 @@
+﻿using Castle.DynamicProxy;
+
+namespace Adnc.Infra.Repository.Interceptor.Castle;
+
+/// <summary>
+/// 工作单元拦截器
+/// </summary>
+public class UowInterceptor(UowAsyncInterceptor uowAsyncInterceptor) : IInterceptor
+{
+    public void Intercept(IInvocation invocation)
+    {
+        uowAsyncInterceptor.ToInterceptor().Intercept(invocation);
+    }
+}
---
+++ src/Infrastructures/Repository/Interceptor/UnitOfWorkAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Adnc.Infra.Repository.Interceptor;
+
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class UnitOfWorkAttribute : Attribute
+{
+    /// <summary>
+    /// 需要把事务共享给CAP
+    /// </summary>
+    public bool Distributed { get; set; }
+}
