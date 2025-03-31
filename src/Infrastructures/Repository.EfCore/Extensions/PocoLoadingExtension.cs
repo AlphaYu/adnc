891c245dbb912b848ca5ec232f1009988a39fde0
Alpha Yu <xyz199809@gmail.com>
2025-03-31 20:49:52 +0800

    chore: Clean up unused code
    - Removed multiple compile items in `Adnc.Infra.Repository.EfCore.csproj`, enabled documentation generation, and added description metadata.
    - Deleted `DbContextExtension` and `MutableEntityTypeExtension` classes along with their related methods.
    - Removed the `EFCoreUtil` class.

---
+++ src/Infrastructures/Repository.EfCore/Extensions/PocoLoadingExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Adnc.Infra.Repository.EfCore.Extensions;
+
+/// <summary>
+/// 延时加载扩展方法
+/// </summary>
+public static class PocoLoadingExtension
+{
+    public static TRelated Load<TRelated>(
+         this Action<object, string> loader,
+         object entity,
+         ref TRelated navigationField,
+         [CallerMemberName] string? navigationName = null)
+         where TRelated : class
+    {
+        loader?.Invoke(entity, navigationName ?? string.Empty);
+
+        return navigationField;
+    }
+}
--- src/Infrastructures/Repository.EfCore/Internal/EFCoreUtil.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Adnc.Infra.Repository.EfCore.Internal;
-
-internal static class EFCoreUtil
-{
-    internal static object[] GetEntityKeyValues<TEntity>(Func<TEntity, object>[] keyValueGetter, TEntity entity)
-        => keyValueGetter.Select(x => x.Invoke(entity)).ToArray();
-}
