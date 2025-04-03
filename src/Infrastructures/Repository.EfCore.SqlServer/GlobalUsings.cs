0704ed198f6124a3e142ec08565866c1879cd079
Alpha Yu <xyz199809@gmail.com>
2025-04-03 20:02:47 +0800

    chore: Remove unused using

--- src/Infrastructures/Repository.EfCore.SqlServer/GlobalUsings.cs
+++ src/Infrastructures/Repository.EfCore.SqlServer/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System.Data;
 global using Adnc.Infra.Repository;
-global using Adnc.Infra.Repository.EfCore;
 global using Adnc.Infra.Repository.EfCore.Transaction;
 global using DotNetCore.CAP;
 global using Microsoft.EntityFrameworkCore;
