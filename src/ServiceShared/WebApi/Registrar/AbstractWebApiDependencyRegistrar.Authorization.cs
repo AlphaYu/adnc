1a8fbd661b836a4715a12c63379265a9b3625521
Alpha Yu <xyz199809@gmail.com>
2026-04-11 00:23:59 +0800

    remove comments

--- src/ServiceShared/WebApi/Registrar/AbstractWebApiDependencyRegistrar.Authorization.cs
+++ src/ServiceShared/WebApi/Registrar/AbstractWebApiDependencyRegistrar.Authorization.cs
@@ -7,8 +7,6 @@ public abstract partial class AbstractWebApiDependencyRegistrar
 {
     /// <summary>
     /// 注册授权组件
-    /// PermissionHandlerRemote 跨服务授权
-    /// PermissionHandlerLocal  本地授权,adnc.usr走本地授权，其他服务走Rpc授权
     /// </summary>
     /// <typeparam name="TAuthorizationHandler"></typeparam>
     protected virtual void AddAuthorization<TAuthorizationHandler>() where TAuthorizationHandler : AbstractPermissionHandler
