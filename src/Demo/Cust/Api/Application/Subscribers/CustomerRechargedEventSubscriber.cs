b67a27396bb3c64d8bb4536e3aeb96466103f7de
Alpha Yu <xyz199809@gmail.com>
2026-04-19 15:55:40 +0800

    chore:remove invalid method HandleOrderPaidEvent

--- src/Demo/Cust/Api/Application/Subscribers/CustomerRechargedEventSubscriber.cs
+++ src/Demo/Cust/Api/Application/Subscribers/CustomerRechargedEventSubscriber.cs
@@ -3,8 +3,7 @@ using Adnc.Infra.EventBus.Tracker;
 
 namespace Adnc.Demo.Cust.Api.Application.Subscribers;
 
-public sealed partial class CustomerRechargedEventSubscriber(IUnitOfWork unitOfWork, IEfRepository<Finance> finaceRepo, IEfRepository<TransactionLog> transactionLogRepo
-    , ILogger<CustomerRechargedEventSubscriber> logger, MessageTrackerFactory trackerFactory) : ICapSubscribe
+public sealed partial class CustomerRechargedEventSubscriber(IUnitOfWork unitOfWork, IEfRepository<Finance> finaceRepo, IEfRepository<TransactionLog> transactionLogRepo, MessageTrackerFactory trackerFactory) : ICapSubscribe
 {
     private readonly IMessageTracker _tracker = trackerFactory.Create();
 
@@ -51,49 +50,6 @@ public sealed partial class CustomerRechargedEventSubscriber(IUnitOfWork unitOfW
             transLog.ChangingAmount = originalBalance;
             transLog.ChangedAmount = changedBalance;
             await transactionLogRepo.UpdateAsync(transLog);
-
-            await _tracker.MarkAsProcessedAsync(eventId, eventHandler);
-
-            await unitOfWork.CommitAsync();
-        }
-        catch (Exception ex)
-        {
-            await unitOfWork.RollbackAsync();
-            throw new InvalidOperationException($"{eventHandler}-{eventId}", ex);
-        }
-        finally
-        {
-            unitOfWork.Dispose();
-        }
-    }
-
-    /// <summary>
-    /// Subscribe to payment events
-    /// </summary>
-    /// <param name="eventDto"></param>
-    /// <returns></returns>
-    [CapSubscribe(nameof(OrderPaidEvent))]
-    public async Task HandleOrderPaidEvent(OrderPaidEvent eventDto)
-    {
-        eventDto.TrimStringFields();
-
-        var eventId = eventDto.Id;
-        var eventHandler = nameof(HandleOrderPaidEvent);
-        var hasProcessed = await _tracker.HasProcessedAsync(eventId, eventHandler);
-        if (hasProcessed)
-        {
-            return;
-        }
-
-        try
-        {
-            unitOfWork.BeginTransaction();
-
-            logger.LogInformation("------Start processing [{eventId}]------", eventId);
-            //TODO
-            //
-            //
-            logger.LogInformation("------Completed processing [{eventId}]------", eventId);
 
             await _tracker.MarkAsProcessedAsync(eventId, eventHandler);
 
