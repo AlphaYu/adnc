ffbc9f8d655a1374a46a0a49c4d08dfbd06eace0
Alpha Yu <xyz199809@gmail.com>
2026-04-20 00:07:51 +0800

    chore:remove adnc.demo.const project

--- src/Demo/Shared/Const/Mq/MqConsts.Exchanges.cs
+++
@@ -1,9 +0,0 @@
-namespace Adnc.Demo.Const.Mq;
-
-public static class MqExchanges
-{
-    public const string Logs = "ex-adnc-logs";
-    public const string Sms = "ex-adnc-sms";
-    public const string Emails = "ex-adnc-emails";
-    public const string Dead = "ex-adnc-dead-letter";
-}
--- src/Demo/Shared/Const/Mq/MqConsts.RoutingKeys.cs
+++
@@ -1,7 +0,0 @@
-namespace Adnc.Demo.Const.Mq;
-
-public static class MqRoutingKeys
-{
-    public const string OpsLog = "opslog";
-    public const string Loginlog = "loginlog";
-}
